a9fd8dee0d8d47be0091882a10cc7844e5009dec
Gabriel Pineda <gabepineda6@gmail.com>
2020-10-20 22:26:11 -0700

    createda department table

--- data.sql
+++ data.sql
@@ -3,7 +3,19 @@ CREATE employeeDB;
 USE employeeDB;
 
 CREATE TABLE employees (
- 
+    id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(45),
+    last_name VARCHAR(45),
+    role_id INT NOT NULL,
+    manager_id INT NOT NULL,
+    PRIMARY KEY (id)
 );
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
 
 SELECT * FROM employees;

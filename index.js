d2e5212983a04b00fcc70001b4c2ddd07d6cbab1
Gabriel Pineda <gabepineda6@gmail.com>
2020-10-20 19:57:14 -0700

    created the starter prompts using inquirer

--- index.js
+++ index.js
@@ -16,3 +16,34 @@ connection.connect(function(err) {
   if (err) throw err;
   Start();
 });
+
+function Start(){
+  inquirer.prompt({
+    type: "list",
+    message: "What do you want to do?",
+    name: "nextStep",
+    choices: [
+      {
+        name: "View All Employees"
+      },
+      {
+        name: "View Departments"
+      },
+      {
+        name: "View Employee Roles"
+      },
+      {
+        name: "Add Employee"
+      },
+      {
+        name: "Add Department"
+      },
+      {
+        name: "Add Employee Role"
+      },
+      {
+        name: "Update Employee Roles"
+      }
+    ]
+  });
+}
